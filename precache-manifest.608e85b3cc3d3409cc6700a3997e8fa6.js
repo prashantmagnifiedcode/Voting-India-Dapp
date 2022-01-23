37548b6a31bcd1c278c4d90c0a118bd7d72f5c16
Prashant Srivastava <82441036+prashantmagnifiedcode@users.noreply.github.com>
2022-01-23 16:54:54 +0530

    Updates

---
+++ precache-manifest.608e85b3cc3d3409cc6700a3997e8fa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bd3b325b130364cccddc13c06f67d5",
+    "url": "/Voting-India-Dapp/index.html"
+  },
+  {
+    "revision": "e40de28f9521c27b3d84",
+    "url": "/Voting-India-Dapp/static/css/main.6375f218.chunk.css"
+  },
+  {
+    "revision": "e796d2bbc35a319a72f0",
+    "url": "/Voting-India-Dapp/static/js/2.9100c889.chunk.js"
+  },
+  {
+    "revision": "e40de28f9521c27b3d84",
+    "url": "/Voting-India-Dapp/static/js/main.d4220cf4.chunk.js"
+  },
+  {
+    "revision": "aa428a0fb0ed31cb31d2",
+    "url": "/Voting-India-Dapp/static/js/runtime-main.cfe57985.js"
+  }
+]);
